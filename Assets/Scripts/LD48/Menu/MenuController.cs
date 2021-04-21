5813dbc0ccb2e9efc797b556f19bb99e9c1c2100
Nathan <nathan.istace@gmail.com>
2021-04-21 23:35:57 +0200

    Ready !

---
+++ Assets/Scripts/LD48/Menu/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using LD48.Menu.Ui;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils.Extensions;
+
+namespace LD48.Menu {
+	public class MenuController : MonoBehaviour {
+		public void Start() {
+			MenuUi.onStartButtonClicked.AddListenerOnce(HandleStartButtonClicked);
+		}
+
+		private static void HandleStartButtonClicked() => SceneManager.LoadSceneAsync("Game");
+	}
+}
---
+++ Assets/Scripts/LD48/Menu/Ui/MenuUi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace LD48.Menu.Ui {
+	public class MenuUi : MonoBehaviour {
+		private static MenuUi instance { get; set; }
+
+		[SerializeField] protected Button _startButton;
+
+		public static UnityEvent onStartButtonClicked => instance._startButton.onClick;
+
+		private void Awake() => instance = this;
+	}
+}
