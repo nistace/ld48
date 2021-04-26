2b5bbd49a15d12c5c9d0c0cc5a07f1551cbbd4a0
Nathan <nathan.istace@gmail.com>
2021-04-26 02:57:46 +0200

    Menu, settings and last fixes

--- Assets/Scripts/LD48/Menu/Ui/MenuUi.cs
+++ Assets/Scripts/LD48/Menu/Ui/MenuUi.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using Utils.Audio;
+using Utils.Extensions;
 
 namespace LD48.Menu.Ui {
 	public class MenuUi : MonoBehaviour {
 		private static MenuUi instance { get; set; }
 
 		[SerializeField] protected Button _startButton;
+		[SerializeField] protected Slider _musicSlider;
+		[SerializeField] protected Slider _sfxSlider;
 
 		public static UnityEvent onStartButtonClicked => instance._startButton.onClick;
 
-		private void Awake() => instance = this;
+		private void Awake() {
+			instance = this;
+			_musicSlider.onValueChanged.AddListenerOnce(t => AudioManager.Music.volume = t);
+			_sfxSlider.onValueChanged.AddListenerOnce(t => AudioManager.Sfx.volume = t);
+		}
+
+		private void Start() {
+			_musicSlider.SetValueWithoutNotify(AudioManager.Music.volume);
+			_sfxSlider.SetValueWithoutNotify(AudioManager.Sfx.volume);
+		}
 	}
 }
